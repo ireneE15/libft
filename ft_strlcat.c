69e42a21ff5df840890355d30ebb07e5114ae152
Irene Escalona Calleja <iescalon@c3r6s4.42urduliz.com>
2024-01-09 12:45:50 +0100

    .

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,16 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/08 14:14:21 by iescalon          #+#    #+#             */
-/*   Updated: 2024/01/09 12:16:38 by iescalon         ###   ########.fr       */
+/*   Created: 2024/01/09 11:44:30 by iescalon          #+#    #+#             */
+/*   Updated: 2024/01/09 12:16:30 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+int	strlen(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcat(const char *src, char *dest, size_t size)
+{
+	size_t	src_lon;
+	size_t	dest_lon;
+	size_t	total_lon;
+
+	src_lon = strlen(src);
+	dest_lon = strlen(dest);
+	total_lon = src_lon + dest_lon
+}
