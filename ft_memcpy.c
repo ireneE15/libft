9353d77c9e6a8d631fd0ed0dcfe6d5bf465b017f
Irene Escalona Calleja <iescalon@c3r5s6.42urduliz.com>
2024-01-09 10:32:11 +0100

    corrección del memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,14 +6,14 @@
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 13:53:32 by iescalon          #+#    #+#             */
-/*   Updated: 2024/01/08 14:11:31 by iescalon         ###   ########.fr       */
+/*   Updated: 2024/01/09 10:29:44 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	const unsigned char	*src2;
 	unsigned char		*dest2;
@@ -30,16 +30,15 @@ void	*memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-/*
+
 int	main(void)
 {
 	const char	source[] = "Hola, mundo!";
 	char		destination[20];
 
-	memcpy(destination, source, 13);
+	ft_memcpy(destination, source, 13);
 	destination[13] = '\0';
 	printf("Source: %s\n", source);
 	printf("Destination: %s\n", destination);
 	return (0);
 }
-*/
