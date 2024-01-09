ac56eae0b5a26f9f7d9c87806be5714fbae55640
Irene Escalona Calleja <iescalon@c3r5s6.42urduliz.com>
2024-01-09 14:34:13 +0100

    .

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,91 @@
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 14:14:21 by iescalon          #+#    #+#             */
-/*   Updated: 2024/01/09 13:51:45 by iescalon         ###   ########.fr       */
+/*   Updated: 2024/01/09 14:31:05 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
-
+/*
 void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = dest;
+	s = src;
+	if (d == s || n == 0)
+	{
+		return (NULL);
+	}
+	if (d < s || d >= (s + n))
+	{
+		while (n--)
+		{
+			*d++ = *s++;
+		}
+	}
+	else
+	{
+		d += n - 1;
+		s += n - 1;
+		while (n--)
+		{
+			*d-- = *s--;
+		}
+	}
+	return (dest);
+}
+*/
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = dest;
+	s = src;
+	if (d < s)
+	{
+		while (n--)
+		{
+			*d++ = *s++;
+		}
+	}
+	else
+	{
+		d += n;
+		s += n;
+		while (n--)
+		{
+			*(--d) = *(--s);
+		}
+	}
+	return (dest);
+}
+
+char	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int main() {
+    char source[] = "Hello, World!";
+    char destination[20];
+
+    // Usando la función ft_memmove para copiar la cadena source en destination
+    ft_memmove(destination, source, ft_strlen(source) + 1);
+
+    // Imprimiendo el resultado
+    printf("Source: %s\n", source);
+    printf("Destination: %s\n", destination);
+
+    return 0;
+}
